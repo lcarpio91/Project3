0213653e09fb686c1392a6048cba2a46fc5a44ec
ernesto-h <127242722+ernesto-h@users.noreply.github.com>
2023-05-22 17:01:21 -0500

    Updated Warehouse.java
    completed addItem();
    completed updateItems();
    added a main method for testing

--- src/com/skillstorm/general/Warehouse.java
+++ src/com/skillstorm/general/Warehouse.java
@@ -14,32 +14,77 @@ Contains a link to navigate back to the home page
 public class Warehouse {
 
 	private int maxCap;
+	
+	// maybe this ArrayList should be changed to a HashMap?
 	private ArrayList<String> items = new ArrayList<>();
 	private String item;
 	
 	
-	public Warehouse(int maxCap, ArrayList<String> items, String item) {
+	public Warehouse(int maxCap, ArrayList<String> items) {
 		this.maxCap = maxCap;
-		this.items = items;
-		this.item = item;
-		
+		this.items = items;		
 	}
+	
+	///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	/////////////////////////////////////////////////////////////MAIN METHOD///////////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	public static void main(String[] args) {
+		
+		
+		ArrayList<String> items = new ArrayList<>();
+
+		Warehouse wh = new Warehouse(20, items);
+		
+		System.out.println(items.size());
+		
+		wh.addItem("item one");
+		
+		System.out.println(items.size());
+		
+		System.out.println(items);
+		
+		wh.updateItem("item one", "updated item one");
+		
+		System.out.println(items);
+
+		
+	}// main
+	///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 	
 	public void displayItems(){
 		// Displays all of the items currently being stored in the warehouse
 	}
 	
-	public void addItem() {
+	public void addItem(String item) {
 		// Feature for adding a new item to the warehouse
+		// maybe use this method in a different class that includes prompts for the user?
+		
+		// this method takes in a String object and adds that object to
+		// the ArrayList belonging to the referenced Warehouse.
+		getItems().add(item);
 		
 	}
 	
 	public void deleteItems() {
 		// Feature for deleting an existing item from the warehouse
+		
 	}
 	
-	public void updateItem() {
+	public String updateItem(String origItem, String updatedItem) {
 		// Feature for updating an existing item in the warehouse
+		// this method takes in the item you want to update and replaces it with 
+		// the item you specify
+		ArrayList<String> items = getItems();
+		
+		if (items.contains(origItem)) {
+			items.remove(origItem);
+			items.add(updatedItem);
+			return origItem + " updated to: " + updatedItem + ".";
+		}else {
+			return origItem + " not found, no changes made.";
+		}
 	}
 	
 	public int getMaxCap() {
