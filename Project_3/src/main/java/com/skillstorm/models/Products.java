7b0c2043bdc5c55ade7b7cdf4abe39f24fda59f5
ernesto-h <127242722+ernesto-h@users.noreply.github.com>
2023-06-14 16:45:36 -0500

    commit
    added models, controllers, repos, and services for Products and Warehouses Tables

---
+++ Project_3/src/main/java/com/skillstorm/models/Products.java
@@ -0,0 +1,112 @@
+package com.skillstorm.models;
+
+import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Products")
+public class Products{
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "Product_ID")
+	private int productId;
+
+	@Column(name = "Product_name")
+	private String productName;
+
+	@Column(name = "Product_Price")
+	private double productPrice;
+
+	@Column(name = "Quantity")
+	private int quantity;
+
+	@Column(name = "UPC")
+	private String upc;
+	
+	@OneToOne
+		@JoinColumn(name = "Warehouse_ID")
+	private Warehouses warehouses;
+
+	public Warehouse1() {}
+
+	public Warehouse1(int productId, String productName, double productPrice, int quantity, String upc) {
+		super();
+		this.productId = productId;
+		this.productName = productName;
+		this.productPrice = productPrice;
+		this.quantity = quantity;
+		this.upc = upc;
+	}
+
+	public int getProductId() {
+		return this.productId;
+	}
+
+	public void setProductId(int productId) {
+		this.productId = productId;
+	}
+
+	public String getProductName() {
+		return this.productName;
+	}
+
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+
+	public double getProductPrice() {
+		return this.productPrice;
+	}
+
+	public void setProductPrice(double productPrice) {
+		this.productPrice = productPrice;
+	}
+
+	public int getQuantity() {
+		return this.quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	public String getUpc() {
+		return this.upc;
+	}
+
+	public void setUpc(String upc) {
+		this.upc = upc;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Warehouse1 other = (Warehouse1) obj;
+		return productId == other.productId && Objects.equals(productName, other.productName)
+				&& Double.doubleToLongBits(productPrice) == Double.doubleToLongBits(other.productPrice)
+				&& quantity == other.quantity && Objects.equals(upc, other.upc);
+	}
+
+	@Override
+	public String toString() {
+		return "Warehouse1 [productId=" + productId + ", productName=" + productName + ", productPrice=" + productPrice
+				+ ", quantity=" + quantity + ", upc=" + upc + "]";
+	}
+
+}
+
+
