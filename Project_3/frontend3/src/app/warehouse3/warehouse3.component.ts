0b7bdb689e655e9aa7bc790d066ee88f84e155bf
isaac10bolanos <isaac10bolanos@gmail.com>
2023-06-14 13:19:39 -0600

    commit

--- Project_3/frontend3/src/app/warehouse3/warehouse3.component.ts
+++ Project_3/frontend3/src/app/warehouse3/warehouse3.component.ts
@@ -11,6 +11,16 @@ import { Router } from '@angular/router';
 })
 export class Warehouse3Component {
   localWarehouse3: any = [];
+  showUpdateForm: boolean = false;
+  showAddForm: boolean = false;
+  selectedProduct: Warehouse3 | null = null;
+
+  choseProductId: number = 0;
+  formId: string = '';
+  formName: string = '';
+  formPrice: string = '';
+  formQuantity: string = '';
+  formUpc: string = '';
 
   constructor(private backendService: BackendService,
               private router: Router) {
@@ -36,5 +46,41 @@ export class Warehouse3Component {
 
   getProductDetails(productId: number): void {
     this.router.navigate(['warehouse3/details/' + productId]);
+  }
+
+  addNewProduct(): void {
+    this.backendService.addProductByBodyW3(new Warehouse3(0, this.formName, Number (this.formPrice), Number (this.formQuantity), this.formUpc))
+        .subscribe(() => this.getAllProductsW3());
+       this.resetForm();
+  }
+
+  updateProduct(): void {
+    this.backendService.updateProductW3(new Warehouse3( Number(this.formId),
+                                                        this.formName,
+                                                        Number(this.formPrice),
+                                                        Number(this.formQuantity),
+                                                        this.formUpc)).subscribe(() => this.getAllProductsW3());
+    this.resetForm();
+  }
+
+  chosenProduct(warehouse3: Warehouse3): void {
+    this.showAddForm = false;
+    this.showUpdateForm = true;
+    this.selectedProduct = { ...warehouse3 };
+    this.formId = String(this.selectedProduct.productId);
+    this.formName = this.selectedProduct.productName;
+    this.formPrice = String(this.selectedProduct.productPrice);
+    this.formQuantity = String(this.selectedProduct.quantity);
+    this.formUpc = this.selectedProduct.upc;
+  }
+
+  resetForm(): void {
+    this.showAddForm = false; 
+    this.showUpdateForm = false;
+    this.formId = '';
+    this.formName = '';
+    this.formPrice = '';
+    this.formQuantity = '';
+    this.formUpc = '';
   }
 }
--- Project_3/frontend3/src/app/warehouse4/warehouse4.component.ts
+++ Project_3/frontend3/src/app/warehouse4/warehouse4.component.ts
@@ -10,6 +10,16 @@ import { Router } from '@angular/router';
 })
 export class Warehouse4Component {
   localWarehouse4: any = [];
+  showUpdateForm: boolean = false;
+  showAddForm: boolean = false;
+  selectedProduct: Warehouse4 | null = null;
+
+  choseProductId: number = 0;
+  formId: string = '';
+  formName: string = '';
+  formPrice: string = '';
+  formQuantity: string = '';
+  formUpc: string = '';
 
   constructor(private backendService: BackendService,
               private router: Router) {
@@ -35,5 +45,41 @@ export class Warehouse4Component {
 
   getProductDetails(productId: number): void {
     this.router.navigate(['warehouse4/details/' + productId]);
+  }
+
+  addNewProduct(): void {
+    this.backendService.addProductByBodyW4(new Warehouse4(0, this.formName, Number (this.formPrice), Number (this.formQuantity), this.formUpc))
+        .subscribe(() => this.getAllProductsW4());
+       this.resetForm();
+  }
+
+  updateProduct(): void {
+    this.backendService.updateProductW4(new Warehouse4( Number(this.formId),
+                                                        this.formName,
+                                                        Number(this.formPrice),
+                                                        Number(this.formQuantity),
+                                                        this.formUpc)).subscribe(() => this.getAllProductsW4());
+    this.resetForm();
+  }
+
+  chosenProduct(warehouse4: Warehouse4): void {
+    this.showAddForm = false;
+    this.showUpdateForm = true;
+    this.selectedProduct = { ...warehouse4 };
+    this.formId = String(this.selectedProduct.productId);
+    this.formName = this.selectedProduct.productName;
+    this.formPrice = String(this.selectedProduct.productPrice);
+    this.formQuantity = String(this.selectedProduct.quantity);
+    this.formUpc = this.selectedProduct.upc;
+  }
+
+  resetForm(): void {
+    this.showAddForm = false; 
+    this.showUpdateForm = false;
+    this.formId = '';
+    this.formName = '';
+    this.formPrice = '';
+    this.formQuantity = '';
+    this.formUpc = '';
   }
 }
