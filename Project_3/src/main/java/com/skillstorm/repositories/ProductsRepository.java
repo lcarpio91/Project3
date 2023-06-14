54fb6fb20888c7feb028bc2bffb58e8afa7d83d8
ernesto-h <127242722+ernesto-h@users.noreply.github.com>
2023-06-14 16:58:41 -0500

    commit repos
    ProductsRepository and WarehousesRepository updated, should be ready to go.

--- Project_3/src/main/java/com/skillstorm/repositories/ProductsRepository.java
+++ Project_3/src/main/java/com/skillstorm/repositories/ProductsRepository.java
@@ -4,19 +4,23 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import com.skillstorm.models.Warehouse4;
+import com.skillstorm.models.Products;
+import com.skillstorm.models.Warehouses;
 
 @Repository
-public interface ProductsRepository extends CrudRepository<Warehouse4, Integer>{
+public interface ProductsRepository extends CrudRepository<Warehouses, Integer>{
 
-	@Query (value = "SELECT * FROM warehouse4 WHERE Product_Id = %?1%", nativeQuery = true)
-	public Iterable<Warehouse4> findByProductId(String searchString);
+	@Query (value = "SELECT * FROM Products WHERE Product_Id = %?1%", nativeQuery = true)
+	public Iterable<Products> findByProductId(String searchString);
 	
 	
-	@Query (value = "SELECT * FROM warehouse4 WHERE Product_name LIKE %?1%", nativeQuery = true)
-	public Iterable<Warehouse4> findByProductName(String searchString);
+	@Query (value = "SELECT * FROM Products WHERE Product_name LIKE %?1%", nativeQuery = true)
+	public Iterable<Products> findByProductName(String searchString);
 	
-	@Query (value = "SELECT * FROM warehouse4 WHERE UPC LIKE %?1%", nativeQuery = true)
-	public Iterable<Warehouse4> findByUPC(String searchString);
+	@Query (value = "SELECT * FROM Products WHERE UPC LIKE %?1%", nativeQuery = true)
+	public Iterable<Products> findByUPC(String searchString);
+	
+	@Query (value = "SELECT count(Product_ID) FROM Products WHERE Warehouse_ID = %?1%", nativeQuery = true)
+	public Iterable<Products> countProducts(String searchString);
 	
 }
