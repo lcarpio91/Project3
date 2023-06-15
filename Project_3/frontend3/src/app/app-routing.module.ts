4ed7e566f351e18a9027c8e6fc404e2940abc193
lcarp <80301370+lcarpio91@users.noreply.github.com>
2023-06-15 17:12:15 -0500

    product 3
    updated

--- Project_3/frontend3/src/app/app-routing.module.ts
+++ Project_3/frontend3/src/app/app-routing.module.ts
@@ -13,6 +13,8 @@ import { Warehouse3detailsComponent } from './productDetails/warehouse3details/w
 import { Warehouse4detailsComponent } from './productDetails/warehouse4details/warehouse4details.component';
 import { ProductsComponent } from './products/products.component';
 import { Products1detailsComponent } from './productDetails/products1details/products1details.component';
+import { Products3detailsComponent } from './productDetails/products3details/products3details.component';
+import { Products3Component } from './products3/products3.component';
 
 
 const routes: Routes = [
@@ -28,7 +30,9 @@ const routes: Routes = [
   {path:'warehouse3/details/:id', component: Warehouse3detailsComponent},
   {path:'warehouse4/details/:id', component: Warehouse4detailsComponent},
   {path:'products', component: ProductsComponent },
-  {path:'products1/details/:id', component: Products1detailsComponent}
+  {path:'products1/details/:id', component: Products1detailsComponent},
+  {path:'products3', component: Products3Component },
+  {path:'products3/details/:id', component: Products3detailsComponent}
 ];
 
 @NgModule({
